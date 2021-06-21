e35c0c0a7fd440b5bf1eb196682e6c4573ac5595
Sai <mail2saishankar@gmail.com>
2021-06-21 17:19:41 +0530

    Completed Encoders/Decoders

--- utility.js
+++ utility.js
@@ -1,10 +1,7 @@
 function display(input){
 document.querySelector('#url').style.display = 'none';
 document.querySelector('#base64').style.display = 'none';
-document.querySelector('#md5').style.display = 'none';
-document.querySelector('#sha1').style.display = 'none';
-document.querySelector('#sha256').style.display = 'none';
-document.querySelector('#sha512').style.display = 'none';
+document.querySelector('#Hashing').style.display = 'none';
 document.querySelector('#'+ input).style.display = 'block';
 }
 function encode(input){
@@ -15,3 +12,29 @@ function decode(input){
     let text = document.querySelector('#'+input).value;
     document.querySelector("#OutputUrl").value = decodeURIComponent(text);
 }
+function base64Encode(input){
+    let text = document.querySelector('#'+input).value;
+    document.querySelector('#OutputString').value = btoa(text);
+}
+function base64Decode(input){
+    let text = document.querySelector('#'+input).value;
+    document.querySelector('#OutputString').value = atob(text);
+}
+
+function hashing(){
+
+    let option = document.querySelector('#hashSelect').value;
+    let text = document.querySelector('#string-hashing-input').value;
+
+switch(option){
+
+    case "md5": document.querySelector('#string-hashing-output').value = window.CryptoJS.MD5(text);
+    break;
+    case "sha1": document.querySelector('#string-hashing-output').value = window.CryptoJS.SHA1(text);
+    break;
+    case "sha256": document.querySelector('#string-hashing-output').value = window.CryptoJS.SHA256(text);
+    break;
+    case "sha512": document.querySelector('#string-hashing-output').value = window.CryptoJS.SHA512(text);
+    break;
+        }
+    }
